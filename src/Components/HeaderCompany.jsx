fce20440098ded0486b6ddcd97932aab479fe1f2
LuuTranAnhKhoa <luutrananhkhoa1902@gmail.com>
2022-06-10 08:56:37 +0700

    Frontend 10/06

--- src/Components/HeaderCompany.jsx
+++ src/Components/HeaderCompany.jsx
@@ -13,8 +13,8 @@ const HeaderCompany = () => {
               <h1 className="text-4xl">LOGO</h1>
               <div className="flex items-center">
                   <ul className="flex text-xl">
-                  <Link to="/employee" className="px-8">EMPLOYEE</Link>
-                  <Link to="/companymanage" className="px-8">MY COMPANY</Link>
+                  <Link to="/employee" className="px-8 hover:text-secondary">EMPLOYEE</Link>
+                  <Link to="/companymanage" className="px-8 hover:text-secondary">MY COMPANY</Link>
                   </ul>
                     <FaUser size="1.5rem" className="mr-4" />
               </div>
