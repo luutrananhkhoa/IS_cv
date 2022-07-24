e3dfa3b6f2e2857f1871f8fd5db05748c69191f1
19521679 <19521679@gm.uit.edu.vn>
2022-07-24 13:15:08 +0700

    dashboard interface

--- src/Locale/setup.js
+++ src/Locale/setup.js
@@ -3,14 +3,21 @@ import { initReactI18next } from 'react-i18next'
 
 import en from './en/index.json'
 import vn from './vi/index.json'
+import dashboardVi from './vi/page/company/dashboard/index.json'
+import custom from './vi/custom.json'
 
 // the translations
 const resources = {
   en: {
     translation: en,
+    custom: custom,
   },
   vi: {
     translation: vn,
+    custom: custom,
+    page: {
+      company: { dashboard: dashboardVi },
+    },
   },
 }
 
@@ -20,6 +27,9 @@ i18n.use(initReactI18next).init({
   debug: true,
   interpolation: {
     escapeValue: false, // not needed for react as it escapes by default
+  },
+  react: {
+    useSuspense: false,
   },
 })
 
--- src/Page/Company/DashboardCompany/index.jsx
+++ src/Page/Company/DashboardCompany/index.jsx
@@ -3,11 +3,14 @@ import styles from './styles.module.scss'
 import DashboardMenu from '@component/DashboardMenu'
 import HeaderDashboard from '@component/HeaderDashboard'
 import ListCertificate from './ListCertificate'
+import { useTranslation } from 'react-i18next'
 
 export default function Index() {
   const [closeMenu, setCloseMenu] = useState(false)
+  const { t } = useTranslation('page', { keyPrefix: 'company.dashboard' })
   return (
     <>
+      {console.log(t('title'))}
       <HeaderDashboard state={[closeMenu, setCloseMenu]}></HeaderDashboard>
       <div className={styles.container}>
         <DashboardMenu state={[closeMenu, setCloseMenu]}></DashboardMenu>
