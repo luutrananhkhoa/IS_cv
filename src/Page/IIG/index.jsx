f3ff3b33123f03eca27218079bd8d62d8919a7b5
LuuTranAnhKhoa <luutrananhkhoa1902@gmail.com>
2022-07-25 08:34:31 +0700

    Pull main

--- src/Page/IIG/index.jsx
+++ src/Page/IIG/index.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import styles from './styles.module.scss';
+import logo from './images/logo_iig.png';
 
 const Index = () => {
     return ( 
         <div className={styles.container}>
+            <div className={styles.containerContent}>
+                <div className={styles.containerSection}>
+                    <div className={styles.sectionFirst}>
+                        <div className={styles.sectionFirst__img}>
+                            <img src={logo} alt="" />
+                        </div>
+                        <h1>IIG Vietnam</h1>
+                    </div>
+                    <h2>Giới thiệu</h2>
+                    <p>Tổ chức Giáo dục IIG Việt Nam là đơn vị hàng đầu tại Việt Nam, Lào, Campuchia và Myanmar về khảo thí, kiểm định và đảm bảo chất lượng các chứng chỉ quốc TOEIC, TOEFL, MOS, IC3, IC3 Spark, MCE, Adobe Certified Professional, Apple Swift, SAT, SSAT, GRE, CFA… </p>
+                    <h2>Đăng ký xác thực chứng chỉ</h2>
+                    <button>Đăng ký</button>
+                </div>          
+            </div>
         </div>
      );
 }
